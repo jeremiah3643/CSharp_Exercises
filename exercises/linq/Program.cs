be3dda1d44f7f3d795909df888a9990b889f502d
Jeremiah <jeremiah3643@gmail.com>
2018-10-10 09:09:20 -0500

    added

--- exercises/linq/Program.cs
+++ exercises/linq/Program.cs
@@ -1,12 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace linq
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace linq {
+    class Program {
+        static void Main (string[] args) {
+            // Find the words in the collection that start with the letter 'L'
+            List<string> fruits = new List<string> () { "Lemon", "Apple", "Orange", "Lime", "Watermelon", "Loganberry" };
+
+            List<string> LFruits = from fruit in fruits
+
         }
     }
 }
